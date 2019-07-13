ceb61d97c0bdf9bc30fd32f1b7e9bf626f1ce7b0
Tristan Rosales <tristanrosales22@gmail.com>
2019-07-13 18:13:31 +0800

    redesign chatbox

--- chatbot-aws-amplify/src/App.js
+++ chatbot-aws-amplify/src/App.js
@@ -3,109 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 import { Interactions } from 'aws-amplify';
 import { ChatFeed, Message } from 'react-chat-ui'
+import { Widget, addResponseMessage, addLinkSnippet, addUserMessage } from 'react-chat-widget';
+import 'react-chat-widget/lib/styles.css';
 
 class App extends Component {
-  state = {
-    input: '',
-    finalMessage: '',
-    messages: [
-      new Message({
-        id: 1,
-        message: "Hello, how can I help you today?",
-      })
-    ]
+  componentDidMount() {
+    document.title = "Big Mike Personal Trainer (HealthTech)"
+    addResponseMessage("Hello, how can I help you today?");
   }
-  _handleKeyPress = (e) => {
-    if (e.key === 'Enter') {
-      this.submitMessage()
-    }
-  }
-  onChange(e) {
-    const input = e.target.value
-    this.setState({
-      input
-    })
-  }
-  async submitMessage() {
-    const { input } = this.state
-    if (input === '') return
-    const message = new Message({
-      id: 0,
-      message: input,
-    })
-    let messages = [...this.state.messages, message]
 
-    this.setState({
-      messages,
-      input: ''
-    })
-    const response = await Interactions.send("BigMikeBotMOBILEHUB", input);
-    const responseMessage = new Message({
-      id: 1,
-      message: response.message,
-    })
-    messages  = [...this.state.messages, responseMessage]
-    this.setState({ messages })
+  handleNewUserMessage = (newMessage) => {
+    if (newMessage === '') return
+    this.submitMsg(newMessage);
   }
+
+  async submitMsg(input) {
+    const response = await Interactions.send("BigMikeBotMOBILEHUB", input);
+    addResponseMessage(response.message);
+  }
+
   render() {
     return (
       <div className="App">
-        <header style={styles.header}>
-          <p style={styles.headerTitle}>Welcome To Your Personal Trainer (HealthTech)!</p>
-        </header>
-        <div style={styles.messagesContainer}>
-        <h2>{this.state.finalMessage}</h2>
-        <ChatFeed
-          messages={this.state.messages}
-          hasInputField={false}
-          bubbleStyles={styles.bubbleStyles}
+        <Widget
+          handleNewUserMessage={this.handleNewUserMessage}
+          profileAvatar={logo}
+          title="Big Mike Personal Trainer (HealthTech)"
+          subtitle="Coach Big Mike Personal Trainer (HealthTech). Fitness partner through Health Technology."
         />
-
-        <input
-          onKeyPress={this._handleKeyPress}
-          onChange={this.onChange.bind(this)}
-          style={styles.input}
-          value={this.state.input}
-        />
-        </div>
       </div>
     );
-  }
-}
-
-const styles = {
-  bubbleStyles: {
-    text: {
-      fontSize: 16,
-    },
-    chatbubble: {
-      borderRadius: 30,
-      padding: 10
-    }
-  },
-  headerTitle: {
-    color: 'white',
-    fontSize: 22
-  },
-  header: {
-    backgroundColor: 'rgb(0, 132, 255)',
-    padding: 20,
-    borderTop: '12px solid rgb(204, 204, 204)'
-  },
-  messagesContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    padding: 10,
-    alignItems: 'center'
-  },
-  input: {
-    fontSize: 16,
-    padding: 10,
-    outline: 'none',
-    width: 350,
-    border: 'none',
-    borderBottom: '2px solid rgb(0, 132, 255)'
-  }
+  } 
 }
 
 export default App;
